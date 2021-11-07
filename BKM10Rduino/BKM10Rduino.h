eeec66f5a91919ff5de92af10f6973aa95fe3dce
Aaron Bonham <aaron.bonham@gmail.com>
2021-11-07 22:00:16 +1100

    add all remaining commands

--- BKM10Rduino/BKM10Rduino.h
+++ BKM10Rduino/BKM10Rduino.h
@@ -1,6 +1,6 @@
 #include <Arduino.h>
 
-#define COMMANDS_SIZE 19
+#define COMMANDS_SIZE 32
 #define IR_INPUT_PIN        7
 #define TX_ENABLE_PIN       2
 #define RX_ENABLE_LOW_PIN   3
@@ -83,7 +83,20 @@ const char nine[] PROGMEM = "9";
 const char del[] PROGMEM = "Del";
 const char ent[] PROGMEM = "Ent";
 const char shiftSel[] PROGMEM = "Shift";
-const char overscan[] PROGMEM = "Overscan";
+const char overscan[] PROGMEM = "Overscan/16:9";
+const char hsync[] PROGMEM = "HSync/Sync";
+const char vsync[] PROGMEM = "VSync/Blue";
+const char mono[] PROGMEM = "Mono/R";
+const char apt[] PROGMEM = "Apt/G";
+const char comb[] PROGMEM = "Comb/B";
+const char f1[] PROGMEM = "F1/F2";
+const char f3[] PROGMEM = "F3/F4";
+const char safe_area[] PROGMEM = "Safe Area/Address";
+const char phase[] PROGMEM = "Phase manual";
+const char chroma[] PROGMEM = "Chroma manual";
+const char bright[] PROGMEM = "Brigtness manual";
+const char contrast[] PROGMEM = "Contrast manual";
+const char degauss[] PROGMEM = "Degauss";
 
 const char * const names[] PROGMEM = {
   power,
@@ -105,6 +118,19 @@ const char * const names[] PROGMEM = {
   ent,
   shiftSel,
   overscan,
+  hsync,
+  vsync,
+  mono,
+  apt,
+  comb,
+  f1,
+  f3,
+  safe_area,
+  phase,
+  chroma,
+  bright,
+  contrast,
+  degauss,
 };
 
 #define BKM_POWER     { 0x01, 0x10 }
@@ -125,7 +151,20 @@ const char * const names[] PROGMEM = {
 #define BKM_DEL       { 0x01, 0x04 }
 #define BKM_ENT       { 0x01, 0x08 }
 #define BKM_SHIFT     { 0x03, 0x01 }
-#define BKM_OVERSCAN  { 0x03, 0x02 }
+#define BKM_OVERSCAN  { 0x03, 0x02 } //OVERSCAN / 16:9
+#define BKM_H_SYNC    { 0x03, 0x04 } //HORIZ SYNC VIEW / SYNC
+#define BKM_V_SYNC    { 0x03, 0x08 } //VERT SYNC VIEW / BLUE ONLY
+#define BKM_MONO      { 0x03, 0x10 } //MONO / R
+#define BKM_APT       { 0x04, 0x01 } //APT / G
+#define BKM_COMB      { 0x04, 0x02 } //COMB / B
+#define BKM_F1        { 0x04, 0x04 } //F1 / F3
+#define BKM_F3        { 0x04, 0x08 } //F2 / F4
+#define BKM_SAFE_AREA { 0x04, 0x10 } //SAFE AREA / ADDRESS
+#define BKM_PHASE     { 0x02, 0x08 } //PHASE MANUAL
+#define BKM_CHROMA    { 0x02, 0x04 } //CHROMA MANUAL
+#define BKM_BRIGHT    { 0x02, 0x02 } //BRIGHT MANUAL
+#define BKM_CONTRAST  { 0x02, 0x01 } //CONTRAST MANUAL
+#define BKM_DEGAUSS   { 0x01, 0x20 } //DEGAUSS
 
 const struct Command commands[] = {
   { BKM_POWER, false, 0 },
@@ -147,6 +186,19 @@ const struct Command commands[] = {
   { BKM_ENT, false, 16 },
   { BKM_SHIFT, false, 17 },
   { BKM_OVERSCAN, false, 18 },
+  { BKM_H_SYNC, false, 19 },
+  { BKM_V_SYNC, false, 20 },
+  { BKM_MONO, false, 21 },
+  { BKM_APT, false, 22 },
+  { BKM_COMB, false, 23 },
+  { BKM_F1, false, 24 },
+  { BKM_F3, false, 25 },
+  { BKM_SAFE_AREA, false, 26 },
+  { BKM_PHASE, false, 27 },
+  { BKM_CHROMA, false, 28 },
+  { BKM_BRIGHT, false, 29 },
+  { BKM_CONTRAST, false, 30 },
+  { BKM_DEGAUSS, false, 31 },
 };
 
 #ifdef USE_PHYSICAL_BUTTONS
@@ -178,3 +230,154 @@ const struct ButtonCommand buttonCommands[] = {
     { BKM_DOWN, buttonDown, BUTTON_DOWN_PIN, true },
 };
 #endif
+
+
+/*** Thank you to the anonymous pastebin hero responsible for providing the control codes ***/
+
+/*
+  https://pastebin.com/aTUWf33J
+  BKM-10R Protocol
+  Version 1 - May 16, 2016
+  License: CC0
+
+  The BKM-10R is a remote control used for Sony BVM CRT televisions. It is
+  required to control some CRTs without built in control, such as the BVM-20F1U.
+
+  The protocol is vaguely similar to the one used to control Sony VCRs:
+  ftp://ftp.sgi.com/sgi/video/rld/vidpage/s9pin.html
+
+  This document currently only applies to the CONTROL connection, not to the
+  REMOTE 1 input/output, though it's expected that they are similar.
+
+  == Pinout ==
+
+  The remote control and CRT are connected with straight through male-male DE9
+  cable.
+
+  Female DB-9, BKM-10R side:
+  1. GND
+  2. /TXD
+  3. RXD
+  4. GND
+  5. +5V
+  6. GND
+  7. TXD
+  8. /RXD
+  9. GND
+
+  Since a straight through cable is used, TXD on the remote side is RXD on the
+  CRT side. +5V is supplied from the CRT to power the remote.
+
+  == Protocol ==
+
+  Full duplex RS-422, 38400 baud, 8N1 (the Sony VCR protocol uses odd parity, but
+  that seems not to be the case here). If you only want to control the CRT,
+  half duplex RS-485 is electrically compatible.
+
+  == Bank ==
+
+  There are multiple "banks" that can be switched via 3 byte commands:
+  0x49 0x45 0x4E - "IEN" - encoders
+  0x49 0x53 0x57 - "ISW" - switches
+  0x49 0x4C 0x45 - "ILE" - leds
+  0x49 0x43 0x43 - "ICC" - ??
+  0x49 0x4d 0x54 - "IMT" - ??
+
+  The remote lazily switches banks when needed.
+
+  == Keypresses ==
+
+  Each keydown sends 0x44, then group, then mask.
+  0x44 <group> <mask>
+
+  The SHIFT key is "just another key" - state is kept on the CRT. Shifted labels
+  are listed after non-shifted labels.
+
+  You must be in the "ISW" bank first.
+
+  <group> <mask> - label
+  0x03 0x01 - SHIFT
+  0x03 0x02 - OVERSCAN / 16:9
+  0x03 0x04 - HORIZ SYNC VIEW / SYNC
+  0x03 0x08 - VERT SYNC VIEW / BLUE ONLY
+  0x03 0x10 - MONO / R
+  0x04 0x01 - APT / G
+  0x04 0x02 - COMB / B
+  0x04 0x04 - F1 / F3
+  0x04 0x08 - F2 / F4
+  0x04 0x10 - SAFE AREA / ADDRESS
+  0x02 0x40 - UP
+  0x02 0x80 - DOWN
+  0x02 0x10 - MENU
+  0x02 0x20 - ENTER
+  0x02 0x08 - PHASE MANUAL
+  0x02 0x04 - CHROMA MANUAL
+  0x02 0x02 - BRIGHT MANUAL
+  0x02 0x01 - CONTRAST MANUAL
+  0x00 0x01 - 0
+  0x00 0x02 - 1
+  0x00 0x04 - 2
+  0x00 0x08 - 3
+  0x00 0x10 - 4
+  0x00 0x20 - 5
+  0x00 0x40 - 6
+  0x00 0x80 - 7
+  0x01 0x01 - 8
+  0x01 0x02 - 9
+  0x01 0x04 - Del
+  0x01 0x08 - Ent
+  0x01 0x10 - POWER (but see below)
+  0x01 0x20 - DEGAUSS
+
+  == POWER ==
+
+  You only need to send a POWER keypress to turn on the monitor, but for some
+  reason the remote does more.
+
+  Power sends five 3-byte sequences:
+  0x49 0x53 0x57 - switch bank
+  0x44 0x01 0x10 - POWER switch
+  (1s pause)
+  0x49 0x43 0x43 - CC bank?
+  0x49 0x4d 0x54 - MT bank?
+  0x44 0x33 0x31 - switch?
+
+  CRT responds with:
+  0x49 0x43 0x43 - CC bank?
+  <full led state report>
+  0x49 0x4d 0x54 - MT bank?
+  <full led state report>
+
+  == Encoders ==
+
+  Dials are rotary encoders that send 3 bytes:
+  0x44 <encoder id id> <ticks>
+
+  Ticks is a two's complement byte indicating how far the dial has turned since
+  the last message. The remote sends messages about every 100ms when the dial
+  is turning.
+
+  You must be in the "IEN" bank first.
+
+  0x03 - PHASE
+  0x02 - CHROMA
+  0x01 - BRIGHT
+  0x00 - CONTRAST
+
+  == LEDs ==
+
+  LEDs use the same groups and masks as keypresses. Every time the CRT needs to
+  change the lights, it changes to the LED bank:
+
+  0x49 0x4c 0x45
+
+  then sends changed switch groups:
+
+  0x44 <group> <mask>
+
+  where <mask> is all of the active LED masks OR'd together.
+
+  == Memory Card ==
+
+  Nobody cares.
+*/
